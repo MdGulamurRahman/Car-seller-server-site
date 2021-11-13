1d7dd74b6dab3397444e160ce0ea5b867562b2b7
Abrarhusamfahim <fahimjahin321@gmail.com>
2021-11-13 12:29:04 +0600

    changed some codes

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function run() {
         res.json(result)
         
     })
-    //UPDATE SINGLE PRODUCT
+    //UPDATE single product
     app.put('/updateOrder/:id', async(req, res)=>{
         const id = req.params.id;
         const filter = {_id: ObjectId(id)};
