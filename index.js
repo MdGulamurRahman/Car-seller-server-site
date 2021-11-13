05bb43ae05975c6b6e70654ae317fdf96e32151c
Abrarhusamfahim <fahimjahin321@gmail.com>
2021-11-13 12:56:54 +0600

    added new codes

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ async function run() {
         res.json(result)
         
     })
-        //DELETE API 
+        //Delete api
      app.delete('/deleteProduct/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
